4aec124a7f4897a46f2d450b325c7db91b56ddcb
vlaba26 <vlaba26@brophybroncos.org>
2023-11-30 15:55:15 -0700

    Newversion used at comp 1 w/ sensors + preliminary auto detection system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonOp.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -10,32 +11,59 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @Autonomous(name = "Auton", group = "Iterative Opmode")
 public class AutonOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
+
         RobotHardware robot = new RobotHardware(hardwareMap, telemetry);
+        double[] CurrentCoords = new double[]{0, 0, 0,
+                robot.LF.getCurrentPosition(), robot.RF.getCurrentPosition(),
+                robot.LB.getCurrentPosition()};
+        RevBlinkinLedDriver.BlinkinPattern pattern;
+
         double dist1;
         double dist2;
+        double r, g, b;
         int pos;
+
         waitForStart();
-        dist1= robot.dSensor0.getDistance(DistanceUnit.INCH);
-        dist2 = robot.dSensor1.getDistance(DistanceUnit.INCH);
-        sleep(500);
-        if (robot.DSCheck(dist1,dist2) ==1){
-            pos = 1;
-
-        } else if (robot.DSCheck(dist1,dist2)==2) {
-            pos =2;
-
-        }else {
+        while (opModeIsActive() ){
+            //farside blue
+            //shift to measure distances
+            CurrentCoords = robot.driveToTile(CurrentCoords, new double[]{0.25, 0, 0}, 1);
+            dist1 = robot.dSensor0.getDistance(DistanceUnit.INCH);
+            dist2 = robot.dSensor1.getDistance(DistanceUnit.INCH);
+            sleep(500);
+        /*
+        //conditionals for each position
+        if (robot.DSCheck(dist1,dist2) ==2){
+            pos = 2;
+            CurrentCoords = robot.driveToTile(CurrentCoords, new double []{0, 0, 0}, 1);
+            sleep(20);
+            CurrentCoords = robot.driveToTile(CurrentCoords, new double []{0, 0, 0}, 1);
+        } else if (robot.DSCheck(dist1,dist2)==3) {
             pos =3;
+            CurrentCoords = robot.driveToTile(CurrentCoords, new double []{0, 0, 0}, 1);
+            sleep(20);
+            CurrentCoords = robot.driveToTile(CurrentCoords, new double []{0, 0, 0}, 1);
+        }else {
+            pos =1;
+            CurrentCoords = robot.driveToTile(CurrentCoords, new double []{0, 0, 0}, 1);
+            sleep(20);
+            //turn to get behind pole in front of spike mark
+            robot.turnDegrees(CurrentCoords, -90);
+            CurrentCoords = robot.driveToTile(CurrentCoords, new double []{4.25, 2.38, 0}, 1);
+        }
+
+
+    */
+
+            //telemetry.addData("position", pos);
+
+            //robot.MoveDirection(0, 0, .5, 1); // Direction (-180 ≤ angle ≤ 180), Turn (-1 ≤ turn ≤ 1), Throttle (1 is max speed possible), time is in seconds
+            //robot.MoveDirection(0, 90, 0.5, 1);
+            //robot.MoveDirection(0, 0, 0.5, 2);
+
+            telemetry.addLine("End of Autonomous");
+            telemetry.update();
 
         }
-        telemetry.addData("position", pos);
-        robot.MoveDirection(0, 0, .5, 1); // Direction (-180 ≤ angle ≤ 180), Turn (-1 ≤ turn ≤ 1), Throttle (1 is max speed possible), time is in seconds
-        robot.MoveDirection(0, 90, 0.5, 1);
-        robot.MoveDirection(0, 0, 0.5, 2);
-
-        telemetry.addLine("End of Autonomous");
-        telemetry.update();
-
     }
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -13,7 +13,6 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 
@@ -29,8 +28,10 @@ public class BasicTeleOp extends LinearOpMode {
 
         // Variables
         double ClawOffset = 0; // offsets Claw starting position
-        int StartingArmPosition = robot.Arm.getCurrentPosition();
-        int ArmHoldPosition = robot.Arm.getCurrentPosition();
+        int StartingArmPosition = robot.Arm0.getCurrentPosition();
+        int ArmHoldPosition = robot.Arm0.getCurrentPosition();
+        int StartingArmPosition1 = robot.Arm1.getCurrentPosition();
+        int ArmHoldPosition1 = robot.Arm1.getCurrentPosition();
         int LinearSlideHold = robot.LinearSlide.getCurrentPosition();
         double ClawTargetPosition = 0.40; // 1 is equal to 180 degrees of rotation
         double r, g, b;
@@ -55,13 +56,19 @@ public class BasicTeleOp extends LinearOpMode {
 
             // Arm
             if (Math.abs(gamepad2.right_stick_y) > 0.05) {
-                robot.Arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                robot.Arm.setPower(-0.5 * gamepad2.right_stick_y);
-                ArmHoldPosition = robot.Arm.getCurrentPosition();
+                robot.Arm0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                robot.Arm0.setPower(-0.5 * gamepad2.right_stick_y);
+                robot.Arm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                robot.Arm1.setPower(-0.5 * gamepad2.right_stick_y);
+                ArmHoldPosition = robot.Arm0.getCurrentPosition();
+                ArmHoldPosition1 = robot.Arm1.getCurrentPosition();
             } else {
-                robot.Arm.setPower(1);
-                robot.Arm.setTargetPosition(ArmHoldPosition + StartingArmPosition);
-                robot.Arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.Arm0.setPower(1);
+                robot.Arm0.setTargetPosition(ArmHoldPosition + StartingArmPosition);
+                robot.Arm0.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.Arm1.setPower(1);
+                robot.Arm1.setTargetPosition(ArmHoldPosition1 + StartingArmPosition1);
+                robot.Arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
 
 
@@ -99,10 +106,11 @@ public class BasicTeleOp extends LinearOpMode {
             robot.colorCheck(r, g, b);
 
 
+
             telemetry.addData("Claw Pos", robot.Claw.getPower());
             telemetry.addData("Intake", robot.Intake.getPower());
             telemetry.addData("LinearSlides", robot.LinearSlide.getCurrentPosition());
-            telemetry.addData("Arm", robot.Arm.getCurrentPosition());
+            telemetry.addData("Arm", robot.Arm0.getCurrentPosition());
             telemetry.addData("LF", robot.LF.getPower());
             telemetry.addData("RF", robot.RF.getPower());
             telemetry.addData("LB", robot.LB.getPower());
@@ -117,6 +125,7 @@ class RobotHardware {
 
     public final HardwareMap map;
     public final Telemetry telemetry;
+    static final double EncoderTickToTile =  1/(( 23.4 * 25.4 * 4096 ) / 109.955742876);
 
 
     public final BNO055IMU imu;
@@ -125,7 +134,7 @@ class RobotHardware {
     public final DcMotor RF, RB, LF, LB;
 
 
-    public final DcMotor Intake, LinearSlide, Arm;
+    public final DcMotor Intake, LinearSlide, Arm0, Arm1;
 
 
     public final CRServo Claw, Plane;
@@ -135,7 +144,19 @@ class RobotHardware {
 
     RevBlinkinLedDriver blinkinLedDriver;
     RevBlinkinLedDriver.BlinkinPattern pattern;
-
+    //c1 and C2 are approx
+    static final double C1 = 2.72824076488, C2 = 1.82212373908, mmToTile =
+            0.00168248199744, EncoderTickToMM = 0.026844663788,
+    //Encoderticktomm is constant and fixed- no calibration
+    //mmtotile is constant + fixed- no calibration
+    //C2 is updated
+    //C1 is not used agian
+            encoderBad = (1 * 4096) / (1 * 4096), // (tiles supposed to go * ticks supposed to go) / (distance told to go * encoder ticks actually gone)
+    //amount of error in encoder for calibration
+    //needs to be updated to our robot
+    encoderTicksPer360 = 3.11784486041 * 22708.224; // encoder wheel distance for 360 degrees rotation in tile lengths * tile to encoder
+    //1st term is maybe updated
+    //2nd term is updated to our encoders
 
     public RobotHardware(HardwareMap hardwareMap, Telemetry telemetry){
         this.telemetry = telemetry;
@@ -159,7 +180,8 @@ class RobotHardware {
         LB = hardwareMap.get(DcMotor.class, "LB");
         Intake = hardwareMap.get(DcMotor.class, "Intake");
         LinearSlide = hardwareMap.get(DcMotor.class, "LinearSlide");
-        Arm = hardwareMap.get(DcMotor.class, "Arm");
+        Arm0 = hardwareMap.get(DcMotor.class, "Arm");
+        Arm1 = hardwareMap.get(DcMotor.class, "Arm1");
         dSensor0= hardwareMap.get(DistanceSensor.class, "DS0");
         dSensor1= hardwareMap.get(DistanceSensor.class, "DS1");
         Claw = hardwareMap.get(CRServo.class, "Claw");
@@ -172,7 +194,8 @@ class RobotHardware {
         LB.setDirection(DcMotor.Direction.REVERSE);
         Intake.setDirection(DcMotor.Direction.REVERSE);
         LinearSlide.setDirection(DcMotor.Direction.FORWARD);
-        Arm.setDirection(DcMotorSimple.Direction.REVERSE);
+        Arm0.setDirection(DcMotorSimple.Direction.REVERSE);
+        Arm1.setDirection(DcMotorSimple.Direction.REVERSE);
 
         Claw.setDirection(CRServo.Direction.FORWARD);
 
@@ -182,15 +205,18 @@ class RobotHardware {
         RF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         RB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         LinearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        Arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Arm0.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Arm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         LF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         LB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         RF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         RB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        Arm.setTargetPosition(Arm.getCurrentPosition());
-        Arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Arm0.setTargetPosition(Arm0.getCurrentPosition());
+        Arm0.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Arm1.setTargetPosition(Arm1.getCurrentPosition());
+        Arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         Intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         LinearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
@@ -200,13 +226,16 @@ class RobotHardware {
         RB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         Intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        Arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Arm0.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Arm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         blinkinLedDriver = hardwareMap.get(RevBlinkinLedDriver.class, "blinkinLedDriver");
 
 
         //declare sensors
         cSensor = hardwareMap.get(ColorSensor.class, "cSensor");
+        cSensor.enableLed(false);
+
 
 
         // Information about what the stuff above does: https://ftc-tricks.com/dc-motors/
@@ -217,8 +246,170 @@ class RobotHardware {
 
 
     } // initializes everything
+    public double[] updateCoords(double[] CurrentCoords) {
+
+        double e1 = (-(LF.getCurrentPosition() * encoderBad) - CurrentCoords[3])
+                * EncoderTickToMM;
+        double e2 = ((RF.getCurrentPosition() * encoderBad) - CurrentCoords[4])
+                * EncoderTickToMM;
+        double e3 = ((LB.getCurrentPosition() * encoderBad) - CurrentCoords[5])
+                * EncoderTickToMM;
+        double ChangeInRotation = 0; //((e1 - e2) / (2 * C1));
+        return new double[]{((e3 + ChangeInRotation * C2) * Math.cos((Math.PI *
+                CurrentCoords[2]) / 180) + ((e1 + e2) / 2) * Math.sin((Math.PI *
+                CurrentCoords[2]) / 180) + (CurrentCoords[0] / mmToTile)) * mmToTile, // X
+                (-1 * (e3 + ChangeInRotation * C2) * Math.sin((Math.PI *
+                        CurrentCoords[2]) / 180) + ((e1 + e2) / 2) * Math.cos((Math.PI *
+                        CurrentCoords[2]) / 180) + (CurrentCoords[1] / mmToTile)) * mmToTile, // Y
+                (ChangeInRotation + CurrentCoords[2]), // Rotation Z
+                -LF.getCurrentPosition() * encoderBad, RF.getCurrentPosition() *
+                encoderBad, LB.getCurrentPosition() * encoderBad}; // Last Encoder Positions
+    }
+
+    public double[] driveToTile(double[] CurrentCoords, double[] TargetCoords, double Speed) {
+        //inputs are arrays, [x,y,turn], [x,y,turn], speed(0-1)
+        CurrentCoords = updateCoords(CurrentCoords);
+        LF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        LB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        RF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        RB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        double Distance = Math.sqrt(Math.pow(TargetCoords[0] - CurrentCoords[0],
+                2) + Math.pow(TargetCoords[1] - CurrentCoords[1], 2));
+        double maxPower = 0, maxSpeed = 0;
+        //macro section of PID loop
+        while (Distance > 0.05) { // ||
+            //Math.abs(angleDifference(CurrentCoords[2], TargetCoords[2])) > 5
+
+            CurrentCoords = updateCoords(CurrentCoords);
+            Distance = Math.sqrt(Math.pow(TargetCoords[0] - CurrentCoords[0], 2)
+
+                    + Math.pow(TargetCoords[1] - CurrentCoords[1], 2));
+
+// Tell motors what to do
+            maxPower = Math.min(1, Math.max(Math.max(Math.abs(TargetCoords[0] -
+                    CurrentCoords[0]), Math.abs(TargetCoords[1] - CurrentCoords[1])), 0.0001));
+            maxSpeed = Math.min(1, Math.max((Distance / 0.75), 0.2)); //
+            //Distance in Tiles when motors start slowing down, minimum speed
+
+            driveWithControllers(-1 * ((TargetCoords[0] - CurrentCoords[0]) /
+
+                            maxPower) * maxSpeed,
+
+                    ((TargetCoords[1] - CurrentCoords[1]) / maxPower) *
+
+                            maxSpeed,
+
+                    0.5 * TargetCoords[2], // 0.5 *
+                    //(angleDifference(TargetCoords[2], CurrentCoords[2]) / 180);
+
+                    Speed);
+            //update telem to help w/ tuning
+            telemetry.addData("Distance", Distance);
+            telemetry.addData("Angle Diff:", angleDifference(CurrentCoords[2], TargetCoords[2]));
+
+            telemetry.addData("X:", CurrentCoords[0]);
+            telemetry.addData("Y:", CurrentCoords[1]);
+            telemetry.addData("Z:", CurrentCoords[2]);
+            telemetry.addData("X Power", -1 * ((TargetCoords[0] - CurrentCoords[0]) / maxPower) * maxSpeed);
+
+            telemetry.addData("Y Power", ((TargetCoords[1] - CurrentCoords[1]) / maxPower) * maxSpeed);
+
+            telemetry.addData("Z Power",0.5 * (angleDifference(TargetCoords[2], CurrentCoords[2]) / 180) * maxSpeed);
+            telemetry.update();
+        }
+        LF.setPower(0);
+        LB.setPower(0);
+        RF.setPower(0);
+        RB.setPower(0);
+// Prevents updating encoders while robot is still moving
+        methodSleep(300);
+        return updateCoords(CurrentCoords);
+    }
+//turning
+    public double[] turnDegrees(double[] CurrentCoords, double degrees) {
+        double leftStart = LF.getCurrentPosition() * encoderBad;
+        double rightStart = RF.getCurrentPosition() * encoderBad;
+        double targetRotation = Math.abs(encoderTicksPer360 * (degrees / 360));
+        while (Math.abs(LF.getCurrentPosition() * encoderBad - leftStart) <
+                targetRotation && Math.abs(RF.getCurrentPosition() * encoderBad - rightStart) <
+                targetRotation) {
+
+            driveWithControllers(0, 0, Math.signum(degrees) * 0.5, 0.5);
+        }
+        driveWithControllers(0, 0, 0, 0);
+        methodSleep(300);
+
+        CurrentCoords[2] = angleDifference(0, degrees + CurrentCoords[2]);
+        return CurrentCoords;
+    }
+
+    public double angleDifference(double CurrentAngle, double TargetAngle) {
+        double result1 = Math.floorMod(Math.round((TargetAngle - CurrentAngle) *
+                100), 360 * 100) * 0.01;
+        double result2 = Math.floorMod(Math.round((TargetAngle - CurrentAngle) *
+                100), -360 * 100) * 0.01;
+        if (Math.abs(result1) <= Math.abs(result2)) return result1;
+        else return result2;
+    }
+
+    public void methodSleep(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+// Wait the set amount of time before stopping motors
+        }
+    }
+
+    public void driveWithControllers(double strafe, double forward, double turn, double throttle) {
+        double max_power = Math.max(1, Math.max(Math.max(
+                Math.abs(-forward - strafe + turn), // LF
+                Math.abs(-forward + strafe + turn) // LB
+        ), Math.max(
+                Math.abs(-forward + strafe - turn), // RF
+                Math.abs(-forward - strafe - turn) // RB
+        )));
+        strafe /= max_power;
+        forward /= max_power;
+        turn /= max_power;
+        LF.setPower(throttle * (-forward - strafe + turn));
+        LB.setPower(throttle * (-forward + strafe + turn));
+        RF.setPower(throttle * (-forward + strafe - turn));
+        RB.setPower(throttle * (-forward - strafe - turn));
 
 
+    }
+    public void colorCheck (double red, double green, double blue){
+        // check for White, Black, yellow, Green, Purple
+
+        if (red > 210 && green > 210 && blue > 210) {
+            pattern = RevBlinkinLedDriver.BlinkinPattern.WHITE;
+        } else if (red < 60 && green < 60 && blue < 60){
+            pattern = RevBlinkinLedDriver.BlinkinPattern.BLACK;
+        } else if  (red > blue && green > blue) {
+            pattern = RevBlinkinLedDriver.BlinkinPattern.YELLOW;
+        }  else if (green > red && green > blue) {
+            pattern = RevBlinkinLedDriver.BlinkinPattern.GREEN;
+        } else if (blue > green && red > green) {
+            pattern = RevBlinkinLedDriver.BlinkinPattern.VIOLET;
+        } else {
+            pattern = RevBlinkinLedDriver.BlinkinPattern.STROBE_RED;
+            // Non pixel colors output strobe red for error
+        }
+        blinkinLedDriver.setPattern(pattern);
+
+
+    }
+    public void fakeColorCheck (boolean up, boolean down, boolean right, boolean left){
+        if (up){pattern = RevBlinkinLedDriver.BlinkinPattern.WHITE;}
+        if (down){pattern = RevBlinkinLedDriver.BlinkinPattern.YELLOW;}
+        if (right){pattern = RevBlinkinLedDriver.BlinkinPattern.GREEN;}
+        if (left){pattern = RevBlinkinLedDriver.BlinkinPattern.VIOLET;}
+    }
+    public int DSCheck (double dista, double distb){
+        if (dista < 24){return 1;}
+        if (distb < 24){return 3;}
+        else{return 2;}
+    }
     public void MoveDirection(double direction, double turn, double throttle, long time) {
         // tile = ~23 inches
 
@@ -249,54 +440,6 @@ class RobotHardware {
         LB.setPower(0);
         RF.setPower(0);
         RB.setPower(0);
-    }
-
-
-    public void driveWithControllers(double strafe, double forward, double turn, double throttle) {
-        double max_power = Math.max(1, Math.max(Math.max(
-                Math.abs(-forward - strafe + turn), // LF
-                Math.abs(-forward + strafe + turn) // LB
-        ), Math.max(
-                Math.abs(-forward + strafe - turn), // RF
-                Math.abs(-forward - strafe - turn) // RB
-        )));
-        strafe /= max_power;
-        forward /= max_power;
-        turn /= max_power;
-        LF.setPower(throttle * (-forward - strafe + turn));
-        LB.setPower(throttle * (-forward + strafe + turn));
-        RF.setPower(throttle * (-forward + strafe - turn));
-        RB.setPower(throttle * (-forward - strafe - turn));
-
-
-    }
-    public void colorCheck (double red, double green, double blue){
-        // check for White, Black, yellow, Green, Purple
-        if (red > 230 && green > 230 && blue > 230) {
-            pattern = RevBlinkinLedDriver.BlinkinPattern.WHITE;
-        } else if (red < 60 && green < 60 && blue < 60){
-            pattern = RevBlinkinLedDriver.BlinkinPattern.BLACK;
-        } else if  (red > blue && green > blue) {
-            pattern = RevBlinkinLedDriver.BlinkinPattern.YELLOW;
-        }  else if (green > red && green > blue) {
-            pattern = RevBlinkinLedDriver.BlinkinPattern.GREEN;
-        } else if (blue > green && red > green) {
-            pattern = RevBlinkinLedDriver.BlinkinPattern.VIOLET;
-        } else {
-            pattern = RevBlinkinLedDriver.BlinkinPattern.STROBE_RED;
-            // Non pixel colors output strobe red for error
-        }
-    }
-    public void fakeColorCheck (boolean up, boolean down, boolean right, boolean left){
-        if (up){pattern = RevBlinkinLedDriver.BlinkinPattern.WHITE;}
-        if (down){pattern = RevBlinkinLedDriver.BlinkinPattern.YELLOW;}
-        if (right){pattern = RevBlinkinLedDriver.BlinkinPattern.GREEN;}
-        if (left){pattern = RevBlinkinLedDriver.BlinkinPattern.VIOLET;}
-    }
-    public int DSCheck (double dista, double distb){
-        if (dista < 24){return 1;}
-        if (distb < 24){return 3;}
-        else{return 2;}
     }
 }
 
